f946ef2e7885244054b6a8aefb10a0e94df4f1be
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-01-26 17:11:27 -0300

    #dev - Inicial Migrations and create database

--- JLBlazor_Ecommerce/Shared/Models/Product.cs
+++ JLBlazor_Ecommerce/Shared/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace JLBlazor_Ecommerce.Shared.Models
         public string? Title { get; set; } 
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
     }
