7986002e7b1a7f9d5a519b336ed9743e4d40219a
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-01-27 12:59:36 -0300

    #dev - make the service for Product in client side

---
+++ JLBlazor_Ecommerce/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using JLBlazor_Ecommerce.Shared;
+using JLBlazor_Ecommerce.Shared.Models;
+using System.Net.Http.Json;
+
+namespace JLBlazor_Ecommerce.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProduct()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product");
+
+            if (result != null && result.Data != null) 
+            {
+                Products = result.Data;
+            }
+        }
+    }
+}
