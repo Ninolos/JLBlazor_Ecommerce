2c20479732b74d27c96bbeb5ba642520c913a10a
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-02-07 17:22:53 -0300

    #dev - add search methods to the client

--- JLBlazor_Ecommerce/Client/Services/ProductService/ProductService.cs
+++ JLBlazor_Ecommerce/Client/Services/ProductService/ProductService.cs
@@ -16,6 +16,8 @@ namespace JLBlazor_Ecommerce.Client.Services.ProductService
         }
         public List<Product> Products { get; set; } = new List<Product>();
         public Product Product { get; set; } = new Product();
+        public string Message { get; set; } = "Loading...";
+
         public async Task GetProducts(string? categoryUrl = null)
         {
             var result = categoryUrl == null ? await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product") 
@@ -34,6 +36,31 @@ namespace JLBlazor_Ecommerce.Client.Services.ProductService
             var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/{productId}");
 
             return result;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/Product/search/{searchText}");
+
+            if (result != null && result.Data != null ) 
+            { 
+                Products = result.Data; 
+            }
+
+            if (Products.Count == 0)
+            {
+                Message = "No Products";
+            }
+
+            ProductsChanged.Invoke();
+            
+        }
+
+        public async Task<List<string>> GetProductSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/Product/searchsuggestions/{searchText}");                       
+
+            return result.Data;
         }
     }
 }
